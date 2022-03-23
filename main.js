813053c9628b3d74b8784122f09bcb3b071f812a
kakarots-bro <chaitra.koneru@creatornation.net>
2022-03-23 17:57:21 +0530

    func

--- main.js
+++ main.js
@@ -25,48 +25,47 @@ const result = () => {
   if (userChoice === computerChoice) {
     document.querySelector("#result").innerHTML = `Result is a Draw.`;
   } else if (userChoice === "rock" && computerChoice === "scissor") {
-    document.querySelector("#result").innerHTML = `Player wins!!!`;
-    document.querySelector("#rock").disabled = true;
-    document.querySelector("#paper").disabled = true;
-    document.querySelector("#scissor").disabled = true;
+    enable();
     confetti({
       particleCount: 550,
     });
     setTimeout(() => {
-      document.querySelector("#rock").disabled = false;
-      document.querySelector("#paper").disabled = false;
-      document.querySelector("#scissor").disabled = false;
+      disable();
       confetti.reset();
     }, 2000);
   } else if (userChoice === "paper" && computerChoice === "rock") {
     document.querySelector("#result").innerHTML = `Player wins!!!`;
-    document.querySelector("#rock").disabled = true;
-    document.querySelector("#paper").disabled = true;
-    document.querySelector("#scissor").disabled = true;
+    enable();
     confetti({
       particleCount: 550,
     });
     setTimeout(() => {
-      document.querySelector("#rock").disabled = false;
-      document.querySelector("#paper").disabled = false;
-      document.querySelector("#scissor").disabled = false;
+      disable();
       confetti.reset();
     }, 2000);
   } else if (userChoice === "scissor" && computerChoice === "paper") {
     document.querySelector("#result").innerHTML = `Player wins!!!`;
-    document.querySelector("#rock").disabled = true;
-    document.querySelector("#paper").disabled = true;
-    document.querySelector("#scissor").disabled = true;
+    enable();
     confetti({
       particleCount: 550,
     });
     setTimeout(() => {
-      document.querySelector("#rock").disabled = false;
-      document.querySelector("#paper").disabled = false;
-      document.querySelector("#scissor").disabled = false;
+      disable();
       confetti.reset();
     }, 2000);
   } else {
     document.querySelector("#result").innerHTML = `Computer wins!!!`;
   }
 };
+
+const disable = () => {
+  document.querySelector("#rock").disabled = false;
+  document.querySelector("#paper").disabled = false;
+  document.querySelector("#scissor").disabled = false;
+}
+
+const enable = () => {
+  document.querySelector("#rock").disabled = true;
+  document.querySelector("#paper").disabled = true;
+  document.querySelector("#scissor").disabled = true;
+}
